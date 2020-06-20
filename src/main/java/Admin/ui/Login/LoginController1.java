3dc4836d4a0256291802ea698f71d02d4a94f1fa
Andrea Mara <dalianamara@yahoo.com>
2020-06-20 11:21:25 +0300

    SDGB-69 Crearea unui formular de autentificare + controller

---
+++ src/main/java/Admin/ui/Login/LoginController1.java
@@ -0,0 +1,45 @@
+package Admin.ui.Login;
+
+import Admin.ui.OTP.LibrarianData;
+import Admin.ui.OTP.*;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class LoginController1 {
+    @FXML
+    private JFXPasswordField password;
+    @FXML
+    private JFXTextField username;
+
+    public void handleLoginButtonAction(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void handleCancelButtonAction(ActionEvent event) {
+        System.exit(0);
+    }
+
+    private void closeStage() {
+        ((Stage) username.getScene().getWindow()).close();
+    }
+
+}
